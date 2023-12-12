b5c63c3839c46f0cf73e8da13e72acf3186560e3
Shashmit kumar <shaashmit@gmail.com>
2023-12-12 22:25:53 +0530

    leeton 1:02

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path"); 
 const leetcodeRouter = require("./route/leetcodeRouter");
 app.use(cors());
 app.use(express.json());
